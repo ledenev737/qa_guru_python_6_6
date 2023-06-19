a1ada1e5911d89f6c068fe0175b94470aff42ff0
skhomuti <skhomuti@gmail.com>
2023-06-19 21:08:45 +0500

    Init commit

---
+++ conditions.py
@@ -0,0 +1,10 @@
+
+# Boolean - 3 состояния
+
+# if/elif/else - на примере кода ответа
+
+# Пустые объекты - false
+
+user_list = []
+
+items_count = 0
---
+++ loops.py
@@ -0,0 +1,33 @@
+import random
+
+
+# While - цикл с предусловием
+# пока пользователь не введет правильный номер, ...
+
+
+required_number = 7
+user_number = random.randint(0, 10)
+
+
+iterations_count = 10
+
+
+# For. Итерируем списки и словари
+
+users = [
+    {"name": "Oleg", "age": 32},
+    {"name": "Sergey", "age": 24},
+    {"name": "Stanislav", "age": 15},
+    {"name": "Olga", "age": 45},
+    {"name": "Maria", "age": 18},
+]
+
+
+# Break/Continue/Else - прерывание цикла
+
+# for i in range - цикл с итератором
+
+# enumerate - возвращает пары (индекс, значение)
+
+
+cities = ["Екатеринбург", "Москва", "Сочи"]
